96f02a2ee236ef764516d1097380f4716a40dfa5
Burdusa-Daniel <136704098+Burdusa-Daniel@users.noreply.github.com>
2024-03-16 11:57:44 +0100

    scelta difficolta facile completata

--- TextAdventure/Livello 2/LivelloDue.java
+++ TextAdventure/Livello 2/LivelloDue.java
@@ -2,31 +2,59 @@ import java.util.*;
 
 public class LivelloDue {
 
-    ArrayList<String []> setDomande = new ArrayList<>();
+    ArrayList<String[]> setDomande = new ArrayList<>();
 
-    public LivelloDue(){
+    public LivelloDue() {
 
     }
 
-    public void domande(){
+    Scanner sc = new Scanner(System.in);
 
-    setDomande = new ArrayList<>();
-    String [] domandeFacili={""};
-    String [] domandeMedie={};
-    String [] domandeDifficili={};
-    setDomande.add(domandeFacili);
-    setDomande.add(domandeMedie);
-    setDomande.add(domandeDifficili);
+    public void domande() {
+        setDomande = new ArrayList<>();
+        String[] domandeFacili = {
+                "1)L'incapsulamento aiuta a nascondere i dettagli interni di implementazione di una classe?",
+                "2)È obbligatorio usare sia getter che setter per ogni attributo privato in una classe per rispettare l'incapsulamento?",
+                "3)Può l'incapsulamento ridurre il rischio di modifiche accidentali allo stato interno di un oggetto?",
+                "4)I metodi pubblici di una classe violano il principio di incapsulamento?",
+                "5)L'incapsulamento limita la capacità di un oggetto di interagire con altri oggetti?" };
+        String[] domandeMedie = {};
+        String[] domandeDifficili = {};
+        setDomande.add(domandeFacili);
+        setDomande.add(domandeMedie);
+        setDomande.add(domandeDifficili);
+        sceltaDifficoltà();
+
     }
 
+    public void sceltaDifficoltà() {
+        System.out.println("scegli una difficoltà: FACILE, , MEDIA , DIFFICILE");
+        String difficoltà = sc.nextLine();
+        switch (difficoltà) {
+            case "facile":
+                System.out.println("hai scelta la modalità facile ");
+                String[] domandeScelte = setDomande.get(0);
+                for (String strings : domandeScelte) {
+                    System.out.println(strings);
+                }
+                break;
+            case "media":
+                System.out.println("hai scelta la modalità media");
+                setDomande.get(1);
+                break;
+            case "difficile":
+                System.out.println("hai scelta la modalità difficile");
+                setDomande.get(2);
+                break;
+            default:
+                break;
+        }
 
-
-public void sceltaDifficoltà(){
-    System.out.println("scegli una difficoltà: FACILE, , MEDIA , DIFFICILE");
-
-}
-/*public void punteggio(){
-    
-} */
+    }
+    /*
+     * public void punteggio(){
+     * 
+     * }
+     */
 
 }
--- TextAdventure/Livello 2/Main.java
+++ TextAdventure/Livello 2/Main.java
@@ -1,7 +1,8 @@
 import java.util.*;
+
 public class Main {
-public static void main(String[] args) {
-    LivelloDue livelloDue = new LivelloDue();
-    
-}    
+    public static void main(String[] args) {
+        LivelloDue livelloDue = new LivelloDue();
+        livelloDue.domande();
+    }
 }

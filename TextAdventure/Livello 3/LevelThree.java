86ac0d59859b9cf9e36bf7ef0e69668bcfd2a42a
Giulia Corrado <giulia.corrado02@gmail.com>
2024-03-16 13:05:27 +0100

    Created class with comments on what to do. Work in progress

---
+++ TextAdventure/Livello 3/LevelThree.java
@@ -0,0 +1,6 @@
+public class LevelThree {
+  // Quindi qui scriviamo i metodi in cui far ciclare un'ArrayList dato
+  // L'ArrayList dato sarà per ogni livello di difficoltà:
+  // 1)Facile - 2)Intermedio - 3)Difficile
+
+}

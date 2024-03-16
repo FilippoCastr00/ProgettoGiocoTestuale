493a953aa6c4b10ac6d665e336eda242133a2487
Giulia Corrado <giulia.corrado02@gmail.com>
2024-03-16 14:15:22 +0100

    Added arraylists + empty methods

--- TextAdventure/Livello 3/LevelThree.java
+++ TextAdventure/Livello 3/LevelThree.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 public class LevelThree {
   // Quindi qui scriviamo i metodi in cui far ciclare un'ArrayList dato
   // L'ArrayList dato sarà per ogni livello di difficoltà:
   // 1)Facile - 2)Intermedio - 3)Difficile
+  ArrayList<String> facile = new ArrayList<String>();
+  ArrayList<String> intermedio = new ArrayList<String>();
+  ArrayList<String> difficile = new ArrayList<String>();
 
+  public String modFacile() {
+    String x = "";
+    return x;
+  }
+
+  public String modIntermedio() {
+    String x = "";
+    return x;
+  }
+
+  public String modDifficile() {
+    String x = "";
+    return x;
+  }
 }

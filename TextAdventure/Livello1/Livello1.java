a50c0b78654d34615ac833703e5a7add6f897e9e
Giuseppe Santagada <g.santagada03@gmail.com>
2024-03-16 14:10:38 +0100

    Commit versione 0.4
    Commit delle 14:10 16-03-2024

--- TextAdventure/Livello1/Livello1.java
+++ TextAdventure/Livello1/Livello1.java
@@ -36,12 +36,14 @@ public class Livello1 {
         System.out.println("quale difficoltà vuoi scegliere?");
         String risposta = sc.nextLine();
 
-        for(int i = 1; i > 0;){
+        for (int i = 1; i > 0;) {
             if (risposta.equalsIgnoreCase("facile")) {
                 System.out.println("In Java, quale parola chiave viene utilizzata per ereditare da una classe?");
                 System.out.println("A)extends \n B)implements \n C)inherit \n D)derive");
-                String sceltaRisposta = sc.nextLine();
-                switch (sceltaRisposta) {
+                String sceltaRisposta1 = sc.nextLine();
+
+                //DOMANDA 1
+                switch (sceltaRisposta1) {
                     case "A":
                         System.out.println("Risposta corretta!");
                         risposteCorrette++;
@@ -67,10 +69,80 @@ public class Livello1 {
                         i = 1;
                         break;
                 }
-    
+
+                //DOMANDA 2 
+                System.out.println("Quale dei seguenti concetti descrive meglio l'ereditarietà?");
+                System.out.println(
+                        "A)il processo di nascondere i dettagli di implementazione \n B)La creazione di interfacce utente grafice \n C)La possibilità di creare una nuova classe basata su una classe esistente \n D)L'esecuzione di codice in parallelo");
+                String sceltaRisposta2 = sc.nextLine();
+                switch (sceltaRisposta2) {
+                    case "A":
+                        System.out.println("Risposta sbagliata");
+                        risposteSbagliate++;
+                        i = 0;
+                        break;
+
+                    case "B":
+                        System.out.println("Risposta sbagliata");
+                        risposteSbagliate++;
+                        i = 0;
+                        break;
+
+                    case "C":
+                        System.out.println("Risposta giusta");
+                        risposteCorrette++;
+                        i = 0;
+                        break;
+
+                    case "D":
+                        System.out.println("Risposta sbagliata");
+                        risposteSbagliate++;
+                        i = 0;
+                        break;
+
+                    default:
+                        System.out.println("Devi inserire una tra le risposte a tua disposizione");
+                        i = 1;
+                        break;
+                }
+
+                //DOMANDA 3
+                System.out.println(
+                        "Qual è il termine utilizzato per indicare la classe da cui un'altra classe eredita in Java?");
+                System.out.println("A)Classe figlia \n B)Superclasse \n C)Sottoclasse \n D)Interfaccia");
+                String sceltaRiposta3 = sc.nextLine();
+                switch (sceltaRiposta3) {
+                    case "A":
+                        System.out.println("Risposta sbagliata");
+                        risposteSbagliate++;
+                        i = 0;
+                        break;
+
+                    case "B":
+                        System.out.println("Risposta corretta");
+                        risposteCorrette++;
+                        i = 0;
+                        break;
+
+                    case "C":
+                        System.out.println("Risposta sbagliata");
+                        risposteSbagliate++;
+                        i = 0;
+                        break;
+
+                    case "D":
+                        System.out.println("Risposta sbagliata");
+                        risposteSbagliate++;
+                        i = 0;
+                        break;
+                    default:
+                        System.out.println("devi scegliere una tra le risposte a tua disposizione");
+                        i = 1;
+                        break;
+                }
             }
         }
-        sc.close();
 
     }
+
 }

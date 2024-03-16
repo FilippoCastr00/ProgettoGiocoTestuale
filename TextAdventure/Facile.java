354acaff5bd77ce79e94415fb4f0bd6f5c62084c
Giuseppe Santagada <g.santagada03@gmail.com>
2024-03-16 11:16:41 +0100

    Commit versione 0.1
    commit delle 11:16 16-03-2024

---
+++ TextAdventure/Facile.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Facile extends Ereditarietà {
+    public ArrayList<String> domandeFacili = new ArrayList<String>();
+    public ArrayList<String> domandaBonus = new ArrayList<String>();
+
+    domandeFacili.add
+
+
+}
